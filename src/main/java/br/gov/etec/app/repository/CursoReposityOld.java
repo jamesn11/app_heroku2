e1bab0c916f4365b5da2fcbaf2ec6813fdd615b0
Francisco Aurizelio <francisco.souza63@etec.sp.gov.br>
2020-05-06 22:33:39 -0300

    retirado impost e delete de classes

--- src/main/java/br/gov/etec/app/repository/CursoReposityOld.java
+++
@@ -1,9 +0,0 @@
-package br.gov.etec.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.gov.etec.app.entity.CursoOld;
-
-public interface CursoReposityOld extends JpaRepository<CursoOld, Long> {
-
-}
